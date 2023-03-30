5cb745c7c0302a1ac70a679d9f5307d000105259
ghvfgfcht <121429925+ghvfgfcht@users.noreply.github.com>
2023-03-30 12:33:19 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+       double one=sc.nextInt();
+       double two=sc.nextInt();
+       System.out.println(one/two);
+    }
+}
