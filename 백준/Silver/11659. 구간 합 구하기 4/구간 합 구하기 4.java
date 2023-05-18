7b00674e42d4441537dfbd09cce5e4f6f505c82c
ghvfgfcht <121429925+ghvfgfcht@users.noreply.github.com>
2023-05-18 20:04:54 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 616 ms, Memory: 52436 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[]args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st=new StringTokenizer(br.readLine()," ");
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		int nlist[]=new int[n+1];
+		int result=0;
+		st=new StringTokenizer(br.readLine()," ");
+		for(int i=1;i<=n;i++) {
+			nlist[i]+=nlist[i-1]+Integer.parseInt(st.nextToken());
+		}
+		for(int i=0;i<m;i++) {
+			st=new StringTokenizer(br.readLine()," ");
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			result=nlist[b]-nlist[a-1];
+			bw.write(result+"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
