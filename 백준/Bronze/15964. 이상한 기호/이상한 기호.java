18844812af8774bf3b939ecab7f774c54e6cd9db
ghvfgfcht <121429925+ghvfgfcht@users.noreply.github.com>
2023-07-13 16:33:16 +0900

    [Bronze V] Title: 이상한 기호, Time: 80 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -6,18 +6,20 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main{ 
-	public static void main(String[]args) throws IOException{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st=new StringTokenizer(br.readLine()," ");
-		int a=Integer.parseInt(st.nextToken());
-		int b=Integer.parseInt(st.nextToken());
-		if(a<=1000 && b<=1000) {
-			int result=(a+b)*(a-b);
-			bw.write(result+"\n");
-		}
-		bw.flush();
-		bw.close();
-		br.close();
+	static int extra_operation(int a, int b) {
+		return ((a+b)*(a-b));
 	}
+   public static void main(String[]args) throws IOException{
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+     StringTokenizer st=new StringTokenizer(br.readLine()," ");
+     int a=Integer.parseInt(st.nextToken());
+     int b=Integer.parseInt(st.nextToken());
+     if(a<=1000&&b<=1000) {
+    	 bw.write(extra_operation(a,b)+"\n");
+     }
+     bw.flush();
+     bw.close();
+     br.close();
+   }
 }
