c01a25a50398f07237841e1dba441a80ca46199b
ghvfgfcht <121429925+ghvfgfcht@users.noreply.github.com>
2023-07-31 00:46:00 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 528 ms, Memory: 294124 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main{ 
+	static Deque<Integer>dq=new LinkedList<>();
+	static int n;
+	static int k;
+    public static void main(String[]args) throws IOException{
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+      StringTokenizer st=new StringTokenizer(br.readLine()," ");
+      n=Integer.parseInt(st.nextToken());
+      k=Integer.parseInt(st.nextToken());
+      for(int i=1;i<=n;i++) {
+    	  dq.offer(i);
+      }
+      bw.write("<");
+      while(!dq.isEmpty()) {
+    	  for(int i=0;i<k-1;i++) {
+    		  dq.offerLast(dq.pollFirst());
+    	  }
+    	  int a=dq.pollFirst();
+    	  if(dq.size()>0) {
+    		  bw.write(a+", ");
+    	  }
+    	  else {
+    		  bw.write(a+">");  
+    	  }
+      }
+      bw.flush();
+      bw.close();
+      br.close();
+    }
+}
