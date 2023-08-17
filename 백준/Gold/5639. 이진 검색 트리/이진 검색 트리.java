e779f6d878a61bfaaaa4cb49eb8c50f47eb43668
Jinha Kim <121429925+ghvfgfcht@users.noreply.github.com>
2023-08-17 12:54:42 +0900

    [Gold V] Title: 이진 검색 트리, Time: 1056 ms, Memory: 29988 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,21 +1,32 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
+public class Main{ 
+    public static void main(String[]args) throws IOException{
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      tree t=new tree(Long.parseLong(br.readLine()));
+      String tmp="";
+      while((tmp=br.readLine())!=null) {
+    	  t.insert_node(t, Long.parseLong(tmp));
+      }
+      t.postorder(t);
+      br.close();
+    }
+}
 class tree{
-	int data;
+	Long data;
 	tree left, right;
-	tree(int data){
+	tree(Long data){
 		this.data=data;
 	}
-	tree new_node(int data) {
+	tree new_node(Long data) {
 		tree node=new tree(data);
 		node.left=null;
 		node.right=null;
 		return node;
 	}
-	tree insert_node(tree root, int data) {
+	tree insert_node(tree root, Long data) {
 		if(root==null) {
 			return new_node(data);
 		}
@@ -27,28 +38,11 @@ class tree{
 		}
 		return root;
 	}
-	
-}
-
-public class Main{ 
-	static void postorder(tree root) {
+	void postorder(tree root) {
 		if(root!=null) {
 			postorder(root.left);
 			postorder(root.right);
 			System.out.println(root.data);
 		}
 	}
-    public static void main(String[]args) throws IOException{
-      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-      tree t=new tree(Integer.parseInt(br.readLine()));
-      String tmp="";
-      while((tmp=br.readLine())!=null) {
-    	  t.insert_node(t, Integer.parseInt(tmp));
-      }
-      postorder(t);
-      bw.flush();
-      bw.close();
-      br.close();
-    }
 }
