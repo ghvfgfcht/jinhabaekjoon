3a03e5c88b2e3710380eedf7f5858f394cd70edc
Jinha Kim <121429925+ghvfgfcht@users.noreply.github.com>
2023-10-02 22:51:49 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 116 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        
+        long result = factorial(N);
+        
+        System.out.println(result);
+        
+        scanner.close();
+    }
+    
+    public static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
